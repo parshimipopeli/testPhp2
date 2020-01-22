96ff00b5e72dfb06e9b79dd97eafb02e1737cfa5
PHILIPPE LAMORSKI <philippe.lamorskipro@gmail.com>
2020-01-22 06:37:35 +0100

    update

--- data/traitementLogin.php
+++ data/traitementLogin.php
@@ -1,11 +1,12 @@
 <?php
 $json = file_get_contents("users.json");
+$donneesJson = json_decode($json);
     $passhash =  password_hash('lam62122phil', PASSWORD_DEFAULT);
 
 if (isset($_POST['submit'])) {
     $mail = HTML_SPECIALCHARS($_post['email']);
     $pwd = HTML_SPECIALCHARS($_POST['password']);
-    $passOk = password_verify($_POST['pass'], $passHash);
+    $passOk = password_verify($_POST['pass'], $donneesJson{'pass'});
 
  if (!$passOk) {
        header('LOCATION: ../login.php');
