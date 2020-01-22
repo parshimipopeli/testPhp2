655cf8aeadb301e4cd41e7e4234a4ebe055cb8b2
Lamorski <philippe.lamorskipro@gmail.com>
2020-01-22 10:46:15 +0100

    update

--- login.php
+++ login.php
@@ -16,13 +16,13 @@ if (isset($_POST['login'])) {
                     $_SESSION['name'] = $users['lastName'];
                     header('Location: index.php');
                 }else{
-                    echo 'votre mot de passe n\' est pas valide';
+                    echo 'mot de passe ou email invalide';
                 }
             }
         }
     }
     if (!$email) {
-        echo 'email invalide';
+        echo 'mot de passe ou email invalide';
     }
 
 }
@@ -30,20 +30,64 @@ if (isset($_POST['login'])) {
 ?>
 
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/app.css">
     <title>Document</title>
 </head>
 <body>
-<form method="POST"  action="">
-    <input class="form-control" type="email" name="email" placeholder="entrez votre email">
-    <input class="form-control" type="password" name="pwd" placeholder="entrez votre mot de passe">
-    <button type="submit" name="login" class="btn btn-primary">entrez</button>
-</form>
+<div class="container">
+    <div class="d-flex justify-content-center h-100">
+        <div class="card">
+            <div class="card-header">
+                <h3>Connectez vous !</h3>
+                <div class="d-flex justify-content-end social_icon">
+                    <span><i class="fab fa-facebook-square"></i></span>
+                    <span><i class="fab fa-google-plus-square"></i></span>
+                    <span><i class="fab fa-twitter-square"></i></span>
+                </div>
+            </div>
+            <div class="card-body">
+                <form method="post">
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-user"></i></span>
+                        </div>
+                        <input type="email" name="email" class="form-control" placeholder="email">
+
+                    </div>
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-key"></i></span>
+                        </div>
+                        <input type="password"  name="pwd" class="form-control" placeholder="password">
+                    </div>
+                    <div class="row align-items-center remember">
+                        <input type="checkbox">Remember Me
+                    </div>
+                    <div class="form-group">
+                        <input type="submit"name="login" value="Login" class="btn float-right login_btn">
+                    </div>
+                </form>
+            </div>
+            <div class="card-footer">
+                <div class="d-flex justify-content-center links">
+                    Don't have an account?<a href="#">Sign Up</a>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <a href="#">Forgot your password?</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
 
